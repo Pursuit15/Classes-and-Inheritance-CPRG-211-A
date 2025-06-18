7116db0ae48cc6f8f634dbc3507a54731b668ea7
taylorb-k <tkbkril@icloud.com>
2025-06-18 09:02:01 -0600

    Add files via upload

---
+++ ModernAppliances/A1ModernAppliances/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace ModernAppliances
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Entry point to program
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        static void Main(string[] args)
+        {
+            ModernAppliances modernAppliances = new MyModernAppliances();
+            ModernAppliances.Options option = ModernAppliances.Options.None;
+            
+            while (option != ModernAppliances.Options.SaveExit)
+            {
+                modernAppliances.DisplayMenu();
+
+                string input = Console.ReadLine();
+                
+                // Better input validation
+                if (!int.TryParse(input, out int optionValue) || !Enum.IsDefined(typeof(ModernAppliances.Options), optionValue))
+                {
+                    Console.WriteLine("Invalid option entered. Please try again.");
+                    Console.WriteLine();
+                    continue;
+                }
+
+                option = (ModernAppliances.Options)optionValue;
+                
+                switch (option)
+                {
+                    case ModernAppliances.Options.Checkout:
+                    {
+                        modernAppliances.Checkout();
+                        break;
+                    }
+                    case ModernAppliances.Options.Find:
+                    {
+                        modernAppliances.Find();
+                        break;
+                    }
+                    case ModernAppliances.Options.DisplayType:
+                    {
+                        modernAppliances.DisplayType();
+                        break;
+                    }
+                    case ModernAppliances.Options.RandomList:
+                    {
+                        modernAppliances.RandomList();
+                        break;
+                    }
+                    case ModernAppliances.Options.SaveExit:
+                    {
+                        modernAppliances.Save();
+                        break;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Invalid option entered. Please try again.");
+                        break;
+                    }
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
